046ae566d27359eca6acf4b97b98e0e8a71e6bcd
Rodrigo <rodrigonpi@hotmail.com>
2022-07-19 20:58:24 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          I Love <strong>Jucemara!</strong>
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <strong>--Delicia--</strong>
         </a>
       </header>
     </div>
